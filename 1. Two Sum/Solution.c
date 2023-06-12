b5d9f9f7b3e54f1da94106910eb2c9adbddc4c04
vinodk2004 <131966904+vinodk2004@users.noreply.github.com>
2023-06-12 12:16:27 +0530

    Add files via upload

---
+++ 1. Two Sum/Solution.c
@@ -0,0 +1,18 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int *arr =(int*)malloc(2*sizeof(int));
+    *returnSize =2;
+    for (int i=0; i<numsSize-1 ;i++){
+        for (int j = i+1 ; i<numsSize ;j++){
+            if (nums[i] + nums[j] == target){
+                arr[0] = i;
+                arr[1] = j;
+                return arr;
+            }
+        }
+    }
+    *returnSize =0;
+    return 0;
+}

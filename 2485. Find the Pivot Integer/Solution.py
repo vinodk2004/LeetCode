901aade7dd590dbe10e9aa1b9b6e201a58123335
Vinod K <131966904+vinodk2004@users.noreply.github.com>
2024-03-13 12:08:54 +0530

    Add files via upload

---
+++ 2485. Find the Pivot Integer/Solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        for i in range(1, n+1):
+            if sum(range(1, i+1)) == sum(range(i, n+1)):
+                return i
+        return -1

5bf7bbfaa4edec00567dcc61fb95cda6f1cd2013
Vinod K <131966904+vinodk2004@users.noreply.github.com>
2025-06-11 07:18:45 +0530

    Create Solution.py
    Runtime: 19ms (Beats 97.36%)
    Memory Usage: 18.05 MB (Beats 96.07%)

---
+++ 463. Island Perimeter/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        perimeter = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 0:
+                    continue
+                
+                else:
+                    perimeter += 4
+                    if i and grid[i-1][j] == 1:
+                            perimeter -= 2
+                    
+                    if j and grid[i][j-1] == 1:
+                            perimeter -= 2
+
+        return perimeter

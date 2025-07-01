8b0ed27354f8fb1bb1bb2f3646be1fff17043966
Vinod K <131966904+vinodk2004@users.noreply.github.com>
2025-07-01 10:19:41 +0530

    Create Solution.py

---
+++ 12. Integer to Roman/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        map = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'),
+            (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), 
+            (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        ]
+
+        result = ''
+
+        for val, sym in map:
+            while num >= val:
+                result += sym
+                num -= val
+        return result

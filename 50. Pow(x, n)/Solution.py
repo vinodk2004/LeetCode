f299069544abfeffdeeb6dfc02956f9c9e567b8a
Vinod K <131966904+vinodk2004@users.noreply.github.com>
2025-07-18 12:58:25 +0530

    Create Solution.py

---
+++ 50. Pow(x, n)/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        elif n < 0:
+            return self.myPow(1/x, -n)
+        
+        if n % 2 == 0:
+            return self.myPow(x*x, n/2)
+        else:
+            return self.myPow(x, n-1) * x

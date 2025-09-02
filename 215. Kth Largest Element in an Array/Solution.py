feca36939d4a6bd4b219d6661242c592b6220e6c
Vinod K <131966904+vinodk2004@users.noreply.github.com>
2025-09-02 18:29:54 +0530

    Update Solution.py

--- 215. Kth Largest Element in an Array/Solution.py
+++ 215. Kth Largest Element in an Array/Solution.py
@@ -1,13 +1,24 @@
 import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        for i in range(len(nums)):
-            nums[i] = -nums[i]
+        min_heap = []
+
+        for num in nums:
+            heapq.heappush(min_heap, num)
+
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+            
+        return min_heap[0]
+
+
+        # for i in range(len(nums)):
+        #     nums[i] = -nums[i]
         
-        heapq.heapify(nums)
+        # heapq.heapify(nums)
 
-        for _ in range(k-1):
-            heapq.heappop(nums)
+        # for _ in range(k-1):
+        #     heapq.heappop(nums)
 
-        return -heapq.heappop(nums)
+        # return -heapq.heappop(nums)
         

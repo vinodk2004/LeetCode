f4b67e73b93cb2f4e8dc784a26b9540b280869a6
Vinod K <131966904+vinodk2004@users.noreply.github.com>
2025-09-20 21:30:50 +0530

    Create Solution.sql

---
+++ 177. Nth Highest Salary/Solution.sql
@@ -0,0 +1,8 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET N = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      select distinct salary from Employee order by salary DESC limit 1 OFFSET N
+  );
+END
